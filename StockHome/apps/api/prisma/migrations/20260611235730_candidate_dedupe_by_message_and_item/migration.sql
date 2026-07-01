a7a6830aadba5c42f5062a1e3faef0398b0842b0
s.yamaoka <s.yamaoka>
2026-07-01 15:15:37 +0900

    リポジトリの引っ越し

--- StockHome/apps/api/prisma/migrations/20260611235730_candidate_dedupe_by_message_and_item/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "import_order_candidates_mail_message_id_key";
-
--- CreateIndex
-CREATE INDEX "import_order_candidates_mail_message_id_idx" ON "import_order_candidates"("mail_message_id");
